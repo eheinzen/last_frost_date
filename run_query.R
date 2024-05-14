f5356cb3c044a5d17e25213989a0a2d662012a23
Ethan Heinzen <heinzen.ethan@mayo.edu>
2024-05-14 11:15:14 -0500

    various updates

--- run_query.R
+++ run_query.R
@@ -90,36 +90,39 @@ frost2 <- together %>%
   mutate(year = year(date)) %>%
   filter(
     year < 2023,
-    date >= "1950-01-01",
+    date >= "1943-01-01",
     date < paste0(year, "-07-01")
   ) %>%
   group_by(year) %>%
   summarize(
     nmiss = sum(is.na(min)),
-    date = max(date[min <= 32])
+    `<= 32` = max(date[min <= 32], na.rm = TRUE),
+    `<= 35` = max(date[min <= 35], na.rm = TRUE)
   ) %>%
   ungroup() %>%
+  pivot_longer(starts_with("<="), names_to = "cutoff", values_to = "date") %>%
   mutate(
     days_in = as.integer(difftime(date, floor_date(date, "year"))),
     md = as.Date("2023-01-01") + days_in
   )
-ggplot(frost2, aes(x = year, y = md)) +
+ggplot(frost2, aes(x = year, y = md, color = cutoff)) +
   geom_point() +
   ylab("Last frost date") +
   xlab("Year") +
   geom_smooth(method = "lm", se = FALSE)
 
-LL <- lm(days_in ~ I(year - 2000), data = frost2)
+LL <- lm(days_in ~ I(year - 2000), data = frost2, subset = cutoff == "<= 32")
 conf <- map_dfr(rev(c(0.5, 0.75, 0.9, 0.95)), function(x) {
   predict(LL, data.frame(year = 2023), interval  = "prediction", level = x) %>%
     as.data.frame() %>%
     mutate(alpha = x, year = 2023)
 }) %>%
   mutate(
+    cutoff = "<= 32",
     upr = as.Date("2023-01-01") + upr,
     lwr = as.Date("2023-01-01") + lwr
   )
-ggplot(frost2, aes(x = year, y = md)) +
+ggplot(filter(frost2, cutoff == "<= 32"), aes(x = year, y = md)) +
   geom_point() +
   ylab("Last frost date") +
   xlab("Year") +
